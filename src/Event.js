1345aee1410977b055fd34a086cf97e56eed3436
evandersloot <edvandersloot@gmail.com>
2021-09-21 09:23:41 -0500

    create Events.js

---
+++ src/Event.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+    state = {
+        showDetails: false 
+    };
+
+
+showDetails() {
+    if(this.state.showDetails === false) {
+        this.setState({showDetails: true})
+    } else {
+        this.setState({showDetails: false})
+    }
+};
+
+buttonText() {
+    if(this.state.showDetails === false) {
+        return "more details"
+    } else { 
+        return "less details"
+    }
+};
+
+eventDetails(event) {
+     if(this.state.showDetails === false) {
+         return ''
+     } else {
+         return event.description
+     }
+}
+
+
+    render() {
+
+        const { event } = this.props;
+
+        return  (
+        <div className="events">
+            <h1 className="event-title">{event.summary}</h1>
+            <h2 className="event-location">{event.location}</h2>
+            <p className="event-start">{new Date(event.start.dateTime).toString()}</p>
+            <p className="event-details">{this.eventDetails(event)}</p>
+            <button className="details-button" 
+                onClick={() => {
+                this.showDetails(event)}}>{this.buttonText()}
+            </button>
+        </div>
+        )
+    }
+}
+
+export default Event;
