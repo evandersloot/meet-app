c7e2a3bf72b6e14d30aa5e0f9a13dc9938e6de39
evandersloot <edvandersloot@gmail.com>
2021-09-22 08:05:48 -0500

    update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -23,6 +23,7 @@ class CitySearch extends Component {
         this.setState({
             query: suggestion
         });
+        this.props.updateEvents(suggestion);
     }
 
     render() {
@@ -41,7 +42,7 @@ class CitySearch extends Component {
                             onClick={() => this.handleItemClicked(suggestion)}
                         >{suggestion}</li>
                     ))}
-                    <li key='all'>
+                    <li onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
                 </ul>
